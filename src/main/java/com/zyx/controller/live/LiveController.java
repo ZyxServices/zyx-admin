aaf8577d0f0c31836474466ba127d19d566c3894
MrDeng <396187665@qq.com>
2016-07-19 10:10:24 +0800

    【modify】 直播 控制器修改

---
+++ src/main/java/com/zyx/controller/live/LiveController.java
@@ -0,0 +1,13 @@
+package com.zyx.controller.live;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by MrDeng on 2016/7/19.
+ */
+@Controller
+@RequestMapping(path = "/v1/live")
+public class LiveController {
+    
+}
