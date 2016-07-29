5756c0f826b942a2483caf852f5ec84535d2185e
396187665@qq.com <MrDeng>
2016-07-29 16:48:52 +0800

    【modify】 BaseMapper  屏蔽Bean扫描

--- src/main/java/com/zyx/mapper/LiveInfoMapper.java
+++ src/main/java/com/zyx/mapper/LiveInfoMapper.java
@@ -1,7 +1,8 @@
 package com.zyx.mapper;
 
+import com.zyx.base.BaseMapper;
 import com.zyx.model.LiveInfo;
 
-public interface LiveInfoMapper extends BaseMapper<LiveInfo> {
+public interface LiveInfoMapper extends BaseMapper<LiveInfo>{
 //    public List<LiveInfoVo> selectPageVos(LiveInfoParm parm);
 }
