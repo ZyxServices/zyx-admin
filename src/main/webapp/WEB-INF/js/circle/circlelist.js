3548f0f63a3cca10f56a89fce1230fc645404e56
fzk <878679630@qq.com>
2016-09-26 16:51:09 +0800

    [update]动态圈子收索

--- src/main/webapp/WEB-INF/js/circle/circlelist.js
+++ src/main/webapp/WEB-INF/js/circle/circlelist.js
@@ -134,31 +134,38 @@ $(function () {
     $("#circle-list-table").bootstrapTable({
         type: 'get',
         url: ("../../circle/circleList"),
-        dataType: "json",
         toolbar: '#toolbar',        //工具按钮用哪个容器
-        queryParams: {
-            start: 0,
-            pageSize: 25
-        },
         striped: true,           //是否显示行间隔色
         cache: true,            //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
         pagination: true,          //是否显示分页（*）
         paginationPreText: "上一页",
         paginationNextText: "下一页",
-        pageNumber: 1,            //初始化加载第一页，默认第一页
-        // 每页的记录行数（*）
+        pageNumber: 0,            //初始化加载第一页，默认第一页
+        pageSize: 10,            //每页的记录行数（*）
         checkbox: true,
         checkboxHeader: "true",
         sortable: true,           //是否启用排序
         sortOrder: "asc",          //排序方式
-        pageList: [25, 50, 100],    //可供选择的每页的行数（*）
-        strictSearch: true,
+        pageList: [10, 25, 50, 100],    //可供选择的每页的行数（*）
+        smartDisplay: false,
         height: 460,            //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
         uniqueId: "id",           //每一行的唯一标识，一般为主键列
         search: true,
-        //  clickToSelect: true,        //是否启用点击选中行
-        //showColumns: true,
-        //showHeader: true,
+        sidePagination: "server",
+        strictSearch: false,        //是否启用模糊收索
+        queryParamsType: "undefined",
+        dataField: "data",
+        queryParams: function queryParams(params) {   //设置查询参数
+            console.log(params)
+            var param = {
+                start: 0,
+                pageSize: params.pageSize,
+                searchText: params.searchText
+                //sortName: params.sortName
+                //sortOrder: params.sortOrder
+            };
+            return param;
+        },
         onLoadSuccess: function (data) {  //加载成功时执行
             console.log(data)
         },
